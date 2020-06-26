1b76c54c1b1d49c0930b410de2ed687c3798a7a4
Ineke Vermeulen <=>
2020-06-26 16:38:53 +0200

    karma -> jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+    imports: [ RouterTestingModule ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavigationComponent
       ],
     }).compileComponents();
   }));
@@ -22,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('song-o-graph');
   });
 
-  it('should render title', () => {
+  it('should contain navigation', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('song-o-graph app is running!');
+    expect(compiled.querySelector('sog-navigation')).toBeTruthy();
   });
 });
