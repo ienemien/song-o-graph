93231945dee934a6078a1f5b27ae98beec5f2610
Ineke Vermeulen <inekevermeulen@gmail.com>
2020-09-01 19:58:18 +0200

    added unit tests

--- src/app/scales/scales.component.spec.ts
+++ src/app/scales/scales.component.spec.ts
@@ -1,5 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { of } from 'rxjs';
 import { ScalesStub } from '../testing-utils/scales-stub';
 import { ScaleService } from '../shared/scale.service';
@@ -8,6 +9,7 @@ import { ScalesComponent } from './scales.component';
 describe('ScalesComponent', () => {
   let component: ScalesComponent;
   let fixture: ComponentFixture<ScalesComponent>;
+  let scalesCompEl: HTMLElement;
   let scaleServiceSpy: jasmine.SpyObj<ScaleService>;
 
   beforeEach(() => {
@@ -16,15 +18,41 @@ describe('ScalesComponent', () => {
 
     TestBed.configureTestingModule({
       declarations: [ScalesComponent],
-      providers: [{ provide: ScaleService, useValue: scaleServiceSpy }, FormBuilder]
+      providers: [{ provide: ScaleService, useValue: scaleServiceSpy }, FormBuilder],
+      imports: [ReactiveFormsModule]
     });
 
     fixture = TestBed.createComponent(ScalesComponent);
     component = fixture.componentInstance;
+    scalesCompEl = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the C scale on initialisation', () => {
+    expect(scaleServiceSpy.getScale).toHaveBeenCalledWith('C');
+  });
+
+  it('should get the scale with the selected tonic', () => {
+    const tonicSelectBox: HTMLSelectElement = scalesCompEl.querySelector('#tonic-select');
+    tonicSelectBox.value = 'B-flat';
+    tonicSelectBox.dispatchEvent(new Event('change'));
+
+    fixture.detectChanges();
+
+    expect(scaleServiceSpy.getScale).toHaveBeenCalledWith('B-flat');
+  });
+
+  it('should display the selected scale', () => {
+    const tonicSelectBox: HTMLSelectElement = scalesCompEl.querySelector('#tonic-select');
+    tonicSelectBox.value = 'B-flat';
+    tonicSelectBox.dispatchEvent(new Event('change'));
+
+    const scaleList: HTMLElement = scalesCompEl.querySelector('#scale > li');
+    console.log(scaleList);
+    // todo: check value of tonic
   });
 });
